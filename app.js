775fe2fb76370d18a61df448b31f192c07aeb9ea
László Tarnai <laszlo.tarnai@LEGO.com>
2022-01-25 22:52:22 +0100

    solved lab

---
+++ app.js
@@ -0,0 +1,17 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// Create routes
+app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about-page.html'));
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works-page.html'));
+app.get('/gallery', (request, response, next) => response.sendFile(__dirname + '/views/gallery-page.html'));
+
+// Server Started
+app.listen(3000, () => console.log('Server is running.'));
